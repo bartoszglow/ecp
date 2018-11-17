1e06de1c7e18160306430e99b00f977cf8c4d78d
Bartosz Glowacki <bartosz.glow@gmail.com>
2018-11-17 22:12:34 +0100

    Tournament page displaying countdown.

--- imports/ui/pages/tournament/tournament.js
+++ imports/ui/pages/tournament/tournament.js
@@ -5,6 +5,7 @@ import { Tournaments } from '/imports/api/tournaments/tournaments.js';
 import '/imports/ui/components/tournaments/info/info.js';
 import '/imports/ui/components/ranking/ranking.js';
 import '/imports/ui/components/players/players.js';
+import '/imports/ui/components/countdown/countdown.js';
 
 import './tournament.html';
 
@@ -16,4 +17,10 @@ Template.App_tournament.helpers({
   tournament() {
     return Tournaments.findOne(FlowRouter.getParam('_id'));
   },
+  isTournamentQueued(tournament) {
+    return tournament && tournament.status === 'queued';
+  },
+  isOwnTournament(tournament) {
+    return tournament && tournament._id === Session.get('editTournament') && tournament.author === Meteor.userId();
+  }
 });

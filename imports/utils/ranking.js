0cb1de453112b8067f75f6c518bd8166e23a825e
Bartosz Glowacki <bartosz.glow@gmail.com>
2018-11-19 19:32:10 +0100

    Ranking utils updated

--- imports/utils/ranking.js
+++ imports/utils/ranking.js
@@ -1,32 +1,51 @@
-const createRanking = function({battles, calculationsType, numberOfLevsToSkip = 0}) {
-  let players = [];
+const createRanking = function(battleInfo) {
+  const {
+    battles,
+    calculationsType,
+    numberOfLevsToSkip = 0,
+    numberOfBattlesInTournament = 1
+  } = battleInfo;
+
+  const players = (battleInfo.players && battleInfo.players.map(player => { return { name: player, results: [] } })) || [];
+
+  const isAllPlayersRanking = players.length === 0;
 
   // Construct players array with array of battle results for each
   battles.forEach(battle => {
     if(battle && battle.results && !battle.results.error) {
-      const results = Object.entries(battle.results);
+      let results = Object.entries(battle.results);
 
-      results && results.forEach(([key, result]) => {
-        const index = players.findIndex(player => player && player.name === result.kuski);
+      // If not all players ranking then filter results to create ranking from selected kuskis only
+      if(!isAllPlayersRanking) {
+        results = results.filter(([key, result]) =>
+          players.findIndex(player => player && player.name.toLowerCase() === result.kuski.toLowerCase()) > -1
+        );
+      }
+
+      results && results.forEach(([key, result], index) => {
+        const playerIndex = players.findIndex(player => player && player.name.toLowerCase() === result.kuski.toLowerCase());
+        const position = index + 1;
         const pointsToGrant = calculatePoints({
           algorithm: calculationsType || 'battle',
-          position: result.number,
-          numberOfPlayers: results.length
+          numberOfPlayers: results.length,
+          position
         });
 
-        if(index > -1) {
-          players[index].results.push({
-            battle: battle.index,
+        if(playerIndex > -1) {
+          players[playerIndex].results.push({
+            battle: battle.playerIndex,
             points: pointsToGrant,
             time: result.time,
+            position
           });
         } else {
           players.push({
             name: result.kuski,
             results: [{
-              battle: battle.index,
+              battle: battle.playerIndex,
               points: pointsToGrant,
               time: result.time,
+              position
             }]
           });
         }
@@ -36,10 +55,9 @@ const createRanking = function({battles, calculationsType, numberOfLevsToSkip = 
 
   players.forEach(player => {
     // Sort results by points
-    const resultsSorted = player.results.slice(0).sort((a, b) => b.points - a.points);
+    const resultsSorted = player.results && player.results.slice(0).sort((a, b) => b.points - a.points);
     const numberOfBattlesPlayed = resultsSorted.length;
-    const numberOfBattlesMissed = battles.length - numberOfBattlesPlayed;
-    const numberOfLevsToNotCalculate = Math.max(0, numberOfLevsToSkip - numberOfBattlesMissed);
+    const numberOfLevsToNotCalculate = Math.max(0, (parseInt(numberOfBattlesPlayed, 10) + parseInt(numberOfLevsToSkip, 10)) - parseInt(numberOfBattlesInTournament, 10));
 
     // Remove worst results based on numberOfLevsToSkip
     const resultsToCalculatePoints = resultsSorted.slice(0, resultsSorted.length - numberOfLevsToNotCalculate);
@@ -50,12 +68,39 @@ const createRanking = function({battles, calculationsType, numberOfLevsToSkip = 
     }, 0);
 
     // Calculate points for all battles
-    player.pointsAll = resultsSorted.reduce((points, battle) => {
+    player.pointsAll = player.results.reduce((points, battle) => {
       return points + battle.points;
+    }, 0);
+
+    // Calculate points
+    player.numberOfWins = player.results.reduce((numberOfWins, battle) => {
+      return battle.position === 1 ? numberOfWins + 1 : numberOfWins;
     }, 0);
 
     player.played = resultsSorted.length;
     player.numberOfLevsCalculated = resultsToCalculatePoints.length;
+  });
+
+  const sortedPlayers = players.sort((a, b) => {
+    if(b.points === a.points) {
+      if(b.numberOfWins === a.numberOfWins) {
+        return b.pointsAll - a.pointsAll;
+      } else {
+        return b.numberOfWins - a.numberOfWins;
+      }
+    } else {
+      return b.points - a.points;
+    }
+  });
+
+  sortedPlayers.forEach((player, index, arr) => {
+    const previousPlayer = arr[index -1];
+
+    if(previousPlayer && previousPlayer.points === player.points) {
+      player.position = previousPlayer.position;
+    } else {
+      player.position = index + 1;
+    }
   });
 
   return players.sort((a, b) => b.points - a.points);

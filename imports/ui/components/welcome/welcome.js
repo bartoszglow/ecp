92c3a2244585b0700892198a30fdb8b03d86cdf5
Bartosz Glowacki <bartosz.glow@gmail.com>
2018-11-30 21:03:14 +0100

    Welcome page changes

--- imports/ui/components/welcome/welcome.js
+++ imports/ui/components/welcome/welcome.js
@@ -1,2 +1,10 @@
+import '/imports/ui/components/countdown/countdown.js';
+
 import './welcome.html';
 import './welcome.scss';
+
+Template.welcome.helpers({
+  PEMdate() {
+    return 1544637600 * 1000
+  }
+});

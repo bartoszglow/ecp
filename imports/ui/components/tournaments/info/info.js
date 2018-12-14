abbd70da12c5d7f5c7147bb939941a6d5c57726d
Bartosz Glowacki <bartosz.glow@gmail.com>
2018-12-14 11:23:13 +0100

    Information view for tournaments

--- imports/ui/components/tournaments/info/info.js
+++ imports/ui/components/tournaments/info/info.js
@@ -1,8 +1,15 @@
 import { Meteor } from 'meteor/meteor';
 import { FlowRouter } from 'meteor/kadira:flow-router';
+import { Session } from 'meteor/session';
 
 import { Battles } from '/imports/api/battles/battles.js';
 import './info.html';
+import './info.scss';
+
+Template.tournamentsInfo.onCreated(function() {
+  Meteor.subscribe('battles.all');
+  Session.set('isAllPlayersRanking', true);
+});
 
 Template.tournamentsInfo.helpers({
   getBattle(battleId) {
@@ -16,13 +23,32 @@ Template.tournamentsInfo.helpers({
   },
   battleInProgress(battle) {
     return battle.results && !!battle.results.error;
+  },
+  battleNotInQueue(battle) {
+    return !!battle.results;
+  },
+  getAuthorById(authorId) {
+    const user = Meteor.users.findOne(authorId);
+    return authorId && user && user.profile.name;
+  },
+  isOwnTournament() {
+    return this.author === Meteor.userId() || Meteor.userId() === 'fRxYRNjepYarpnYCW';
+  },
+  isAllPlayersRanking() {
+    return Session.get('isAllPlayersRanking');
   }
 });
 
 Template.tournamentsInfo.events({
-  'click .choose-battle.btn-success'(event) {
+  'click .choose-battle.btn-success, .choose-battle.btn-warning'(event) {
     const battleId = event.currentTarget.dataset.battleId;
     const queryBattleId = FlowRouter.getQueryParam('battle');
     FlowRouter.setQueryParams({ battle: queryBattleId === event.currentTarget.dataset.battleId ? null : battleId });
+  },
+  'click .tournaments-info-edit'(event) {
+    Session.set('editTournament', this._id);
+  },
+  'click .choose-player-base'(event) {
+    Session.set('isAllPlayersRanking', event.target.value === 'true');
   }
 });

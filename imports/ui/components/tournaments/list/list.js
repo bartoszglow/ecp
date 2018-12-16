0dacbbdbeb347f120919718087568efc234b8d6e
Bartosz Glowacki <bartosz.glow@gmail.com>
2018-12-16 10:31:51 +0100

    Tournaments list updated.

--- imports/ui/components/tournaments/list/list.js
+++ imports/ui/components/tournaments/list/list.js
@@ -1,7 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 import { FlowRouter } from 'meteor/kadira:flow-router';
+import moment from 'moment';
 
 import { Tournaments } from '/imports/api/tournaments/tournaments.js';
+
+import '/imports/ui/components/countdown/countdown.js';
+
 import './list.html';
 import './list.scss';
 
@@ -12,7 +16,7 @@ Template.tournamentsList.onCreated(function () {
 Template.tournamentsList.helpers({
   tournaments() {
     const tournaments = Tournaments.find({}).fetch();
-    const sortedTournaments = tournaments && tournaments.sort((a, b) => a.startingDate || a.createdAt > b.startingDate || b.createdAt ? 1 : -1);
+    const sortedTournaments = tournaments && tournaments.sort((a, b) => (moment(a.startingDate).unix() * 1000) > (moment(b.startingDate).unix() * 1000));
 
     return sortedTournaments && [...sortedTournaments.filter(t => t.status !== 'finished'), ...sortedTournaments.filter(t => t.status === 'finished')];
   },
@@ -24,6 +28,32 @@ Template.tournamentsList.helpers({
   },
   getLeader(tournament) {
     return tournament.ranking && tournament.ranking[0] && tournament.ranking[0].name;
+  },
+  getAuthorById(authorId) {
+    const user = Meteor.users.findOne(authorId);
+    return authorId && user && user.profile.name;
+  },
+  finishedTournaments() {
+    const tournaments = Tournaments.find({}).fetch();
+    const sortedTournaments = tournaments && tournaments.sort((a, b) => moment(a.startingDate).unix() > moment(b.startingDate).unix());
+
+    return sortedTournaments.filter(t => t.status === 'finished');
+  },
+  inprogressTournaments() {
+    const tournaments = Tournaments.find({}).fetch();
+    const sortedTournaments = tournaments && tournaments.sort((a, b) => moment(a.startingDate).unix() > moment(b.startingDate).unix());
+
+    return sortedTournaments.filter(t => t.status === 'in progress');
+  },
+  queuedTournaments() {
+    const tournaments = Tournaments.find({}, { title: 1 }).fetch();
+    const sortedTournaments = tournaments.sort((a, b) => moment(a.startingDate).unix() - moment(b.startingDate).unix());
+
+    return sortedTournaments.filter(t => t.status === 'queued');
+  },
+  getAuthorById(authorId) {
+    const user = Meteor.users.findOne(authorId);
+    return authorId && user && user.profile.name;
   }
 });
 
@@ -50,5 +80,8 @@ Template.tournamentsList.events({
   },
   'click .ecp__tournament-create'() {
     FlowRouter.go(`/tournament-create`);
+  },
+  'click .open-tournament'(event, template) {
+    event.target.dataset.id && FlowRouter.go(`/tournament/${event.target.dataset.id}`);
   }
 });
